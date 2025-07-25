c58b8a0eed53c38440cbdb794c3394febc4fd4b2
Anish Dhomase <anishdhomase@gmail.com>
2025-07-25 21:35:47 +0530

    init(userModel): create user model. Implement hashing and jwt token logic
    Created user model. Implemented logic for hashing & comparing password using bcrypt library. Implemented logic for generation of jwt token using jsonwebtoken library

---
+++ Backend/models/user.model.js
@@ -0,0 +1,59 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const userSchema = new mongoose.Schema({
+  fullname: {
+    firstname: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: [2, "First name must be at least 2 characters long"],
+    },
+    lastname: {
+      type: String,
+      trim: true,
+      minlength: [2, "Last name must be at least 2 characters long"],
+    },
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    validate: {
+      validator: function (v) {
+        return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(v);
+      },
+      message: "Please enter a valid email address",
+    },
+    minlength: [5, "Email must be at least 5 characters long"],
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: [6, "Password must be at least 6 characters long"],
+    select: false, // Exclude password from queries by default
+  },
+  socketId: {
+    type: String,
+  },
+});
+
+userSchema.methods.generateAuthToken = function () {
+  const token = jwt.sign({ _id: this._id }, process.env.JWT_SECRET);
+  return token;
+};
+
+userSchema.methods.comparePassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.statics.hashPassword = async function (password) {
+  return await bcrypt.hash(password, 10);
+};
+
+const userModel = mongoose.model("user", userSchema);
+
+module.exports = userModel;
